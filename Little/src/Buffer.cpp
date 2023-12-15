ff712edb8f08ef17a25262b59990eb546dc5bb23
nesmy <pnesmy@gmail.com>
2023-12-14 23:06:28 -0500

    buffer

---
+++ Little/src/Buffer.cpp
@@ -0,0 +1,34 @@
+#include "lepch.h"
+#include "Renderer/Buffer.h"
+
+#include "Renderer/Renderer.h"
+
+#include "Platform/OpenGL/OpenGLBuffer.h"
+
+namespace Little {
+
+
+	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::None:		LE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::OpenGl:	return new OpenGLVertexBuffer(vertices, size);
+		}
+
+		LE_CORE_ASSERT(false, "Unknown RenderAPI");
+		return nullptr;
+	}
+
+	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::None:		LE_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::OpenGl:	return new OpenGLIndexBuffer(indices, size);
+		}
+
+		LE_CORE_ASSERT(false, "Unknown RenderAPI");
+		return nullptr;
+	}
+}
---
+++ Little/src/Renderer.cpp
@@ -0,0 +1,8 @@
+#include "lepch.h"
+#include "Renderer/Renderer.h"
+
+
+namespace Little {
+
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGl;
+}
